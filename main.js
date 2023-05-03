009beff7b9afe207a806d659c5950477e003623e
Abel Matunda Morara <33475814+ProgramKingAbel@users.noreply.github.com>
2023-05-03 16:56:51 +0100

    Added Event listeners

--- main.js
+++ main.js
@@ -160,3 +160,32 @@ document.querySelector('#book-list').addEventListener('click', e => {
   // Show success message
   UI.showAlert('Book Removed', 'success');
 });
+const listBtn = document.querySelector('.list');
+const addBtn = document.querySelector('.add-book');
+const contact = document.querySelector('.contact');
+
+const toList = document.querySelector('table');
+const toAdd = document.querySelector('form');
+const toContact = document.querySelector('#contact');
+
+listBtn.addEventListener('click', e => {
+  e.preventDefault();
+  toList.classList.remove('d-none');
+  toContact.classList.add('d-none');
+  toAdd.classList.add('d-none');
+});
+
+// Go to add list
+addBtn.addEventListener('click', e => {
+  e.preventDefault();
+  toAdd.classList.remove('d-none');
+  toList.classList.add('d-none');
+  toContact.classList.add('d-none');
+});
+// Go to contactas
+contact.addEventListener('click', e => {
+  e.preventDefault();
+  toContact.classList.remove('d-none');
+  toList.classList.add('d-none');
+  toAdd.classList.add('d-none');
+});
