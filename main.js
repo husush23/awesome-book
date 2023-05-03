ccf80002a85ffc9b43d6d460bc09458672cc823e
husush23 <husseinkadare40@gmail.com>
2023-05-03 15:04:57 +0300

    Files added

--- main.js
+++ main.js
@@ -45,7 +45,7 @@ class UI {
   static displayBooks() {
     const books = Store.getBooks();
 
-    books.forEach((book) => UI.addBookToList(book));
+    books.forEach(book => UI.addBookToList(book));
   }
 
   static addBookToList(book) {
@@ -56,7 +56,7 @@ class UI {
     row.innerHTML = `
           <td>${book.title}</td>
           <td>${book.author}</td>
-          <td><a href="#" class="btn btn-danger btn-sm delete">Remove</a></td>
+          <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
         `;
 
     list.appendChild(row);
@@ -91,7 +91,7 @@ class UI {
 document.addEventListener('DOMContentLoaded', UI.displayBooks);
 
 // Event: Add a Book
-document.querySelector('#book-form').addEventListener('submit', (e) => {
+document.querySelector('#book-form').addEventListener('submit', e => {
   // Prevent actual submit
   e.preventDefault();
 
@@ -121,38 +121,8 @@ document.querySelector('#book-form').addEventListener('submit', (e) => {
   }
 });
 
-// Event: Add a Book
-document.querySelector('#book-form').addEventListener('submit', (e) => {
-  // Prevent actual submit
-  e.preventDefault();
-
-  // Get form values
-  const title = document.querySelector('#title').value;
-  const author = document.querySelector('#author').value;
-  const isbn = document.querySelector('#isbn').value;
-
-  // Validate
-  if (title === '' || author === '') {
-    UI.showAlert('Please fill in all fields', 'danger');
-  } else {
-    // Instatiate book
-    const book = new Book(title, author, isbn);
-
-    // Add Book to UI
-    UI.addBookToList(book);
-
-    // Add book to store
-    Store.addBook(book);
-
-    // Show success message
-    UI.showAlert('Book Added', 'success');
-
-    // Clear fields
-    UI.clearFields();
-  }
-});
 // Event: Remove a Book
-document.querySelector('#book-list').addEventListener('click', (e) => {
+document.querySelector('#book-list').addEventListener('click', e => {
   // Remove book from UI
   UI.deleteBook(e.target);
 
